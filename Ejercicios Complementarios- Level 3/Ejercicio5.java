1d9393717b69a0b9cb3a68924077df8b79fe9380
Daiana Martinez <89087443+daianalm@users.noreply.github.com>
2021-11-26 23:57:13 -0300

    Update Ejercicio5.java
    corrección errores

--- Ejercicios Complementarios- Level 3/Ejercicio5.java
+++ Ejercicios Complementarios- Level 3/Ejercicio5.java
@@ -1,25 +1,26 @@
 import java.time.LocalDate;
-import java.util.HashMap;
+import java.time.Period;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Ejercicio5 {
-
-  public static void main(String[] args) {
-    List<Alumno> alumnos =
-        List.of(
-            new Alumno("Homero", "Simpson", LocalDate.of(1925, 11, 1)),
-            new Alumno("Romina", "Martinez", LocalDate.of(1986, 11, 24)),
-            new Alumno("Julia", "Martinez", LocalDate.of(1980, 2, 19)),
-            new Alumno("Daiana", "Martinez", LocalDate.of(1983, 1, 20)),
-            new Alumno("Nancy", "Hermosilla", LocalDate.of(1974, 7, 7)),
-            new Alumno("Federico", "Martinez", LocalDate.of(1980, 9, 1)));
-    Map<String, Integer> edades = new HashMap<>();
-    for (Alumno alumno : alumnos) {
-      edades.put(
-          alumno.getApellido() + " " + alumno.getNombre(),
-          alumno.getNacimiento().until(LocalDate.now()).getYears());
+    public static void main(String[] args) {
+        List<Alumno> alumnos = List.of(
+                new Alumno("Homero", "Simpson", LocalDate.now().minusYears(30)),
+                new Alumno("Daiana", "Simpson", LocalDate.of(1956, 5, 12)),
+                new Alumno("Federico", "Simpson", LocalDate.of(1990, 2, 10)),
+                new Alumno("Romina", "Simpson", LocalDate.of(2001, 3, 30)),
+                new Alumno("Nancy", "Bouvier", LocalDate.of(1971, 2, 5)),
+                new Alumno("Luisina", "Monroe", LocalDate.of(1945, 7, 21)),
+                new Alumno("Jose", "Simpson", LocalDate.of(1970, 11, 11)));
+        Map<String, Integer> alumnosConEdades = alumnos.stream()
+                .collect(Collectors
+                        .toMap(p-> (p.getName() + " " + p.getSurname()),
+                                p -> Ejercicio5.getEdad(p.getBirthday())));
+        System.out.println(alumnosConEdades);
     }
-    System.out.println(edades);
-  }
+    public static Integer getEdad(LocalDate birthDate){
+        return Period.between(birthDate, LocalDate.now()).getYears();
+    }
 }
